049ddf79c4386b3b7dff6468685c3e7cb3fcbba1
PTD2315 <ptd2315@students.iitmandi.ac.in>
2025-03-04 08:49:00 +0530

    Added debug

--- mysqrtplain.py
+++ mysqrtplain.py
@@ -1,6 +1,15 @@
-x=12.0
-s=1.0
-kmax=12
-for k in range(kmax):
-	print (f"At Iteration {k}, the value of s={s}")
-	s=0.5*(s+x/s)
+def mysqrtfunc(x, debug=False):
+	s=1.0
+	kmax=12
+	tol=1.0e-14
+	for k in range(kmax):
+		if(debug):
+			print (f"At Iteration {k}, the value of s={s:20.15f}")
+		s0=s
+		s=0.5*(s+x/s)
+		delta_s=s-s0
+		if(abs(delta_s)/x < tol):
+			break
+	if(debug):
+		print(f"Finally!The value of S is:{s:20.15f}")
+	return s
